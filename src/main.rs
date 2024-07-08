72de9430d8fb8a193c1b219967e7d691585bb5cf
gbubemi22 <slimnfine22@gmail.com>
2024-07-08 12:54:39 +0100

    logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod models;
 mod services;
 
+use actix_web::middleware::Logger;
 use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use env_logger::Env;
 use services::database_config::Database;
 
 #[get("/")]
@@ -11,14 +14,26 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Load environment variables from .env file
+    dotenv().ok();
+
+    // Initialize logger with environment variable support
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Initialize the database connection
     let db = Database::init().await.expect("error");
     // Clone the database connection to share it across handlers
     let db_data = Data::new(db);
 
     // Start the HTTP server
-    HttpServer::new(move || App::new().app_data(db_data.clone()).service(hello))
-        .bind(("localhost", 5001))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .app_data(db_data.clone())
+            .service(hello)
+    })
+    .bind(("localhost", 5001))?
+    .run()
+    .await
 }
